647544b785da6d885d8b085accf51b156c8ac487
Andrey Bushik <andrey.bushik@gmail.com>
2018-06-26 09:05:30 +0300

    generate 50k per iteration instead of 100k

--- generator-json.sh
+++ generator-json.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-NODE_OPTIONS="--max-old-space-size=5120" node -v
-for i in `seq 0 49`; do
-    n=$((i * 100000))
+for i in `seq 0 99`; do
+    n=$((i * 50000))
     sed "s/%customer_id%/${n}/g" ./models/customers_template.yaml > ./models/customers_generated.yaml
     sed "s/%order_id%/${n}/g" ./models/orders_template.yaml > ./models/orders_generated.yaml
 
     echo " >> generating customers and orders for ids from ${n}"
-    bin/fakeit -m ./models/customers_generated.yaml,./models/orders_generated.yaml -n 100000 -o json -d ./results/$i
+    bin/fakeit -m ./models/customers_generated.yaml,./models/orders_generated.yaml -n 50000 -o json -d ./results/$i
     echo " >> done for ids starting from ${n}"
 done
