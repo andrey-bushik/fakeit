f31819511238944b66d3a1d8475221ec23c0fe14
Andrey Bushik <andrey.bushik@gmail.com>
2018-06-27 01:11:24 +0300

    update import scripts to use combined json

--- importer-json-5.sh
+++ importer-json-5.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-folders=100
-records=50000
-for i in `seq 1 $folders`; do
+folders=99
+for i in `seq 0 $folders`; do
     echo "$(date) >> importing folder ${i}..."
-    for j in `seq 0 $records`; do
-        n=$((i * $records + j))
-
-        mongoimport --quiet --mode merge --collection customer --file ./results/$i/customer---$n.json --db ycsb --username guest --password guest --authenticationDatabase admin --host c1:27117
-        mongoimport --quiet --mode merge --collection order --file ./results/$i/order---$n.json --db ycsb --username guest --password guest --authenticationDatabase admin --host c1:27117
-    done
+    mongoimport --jsonArray --mode merge --collection customer --file ./results/$i/combined-customer.json --db ycsb --username guest --password guest --authenticationDatabase admin --host c1:27117
+    mongoimport --jsonArray --mode merge --collection order --file ./results/$i/combined-order.json --db ycsb --username guest --password guest --authenticationDatabase admin --host c1:27117
 done
+
+#folders=100
+#records=50000
+#for i in `seq 1 $folders`; do
+#    echo "$(date) >> importing folder ${i}..."
+#    for j in `seq 0 $records`; do
+#        n=$((i * $records + j))
+#
+#        mongoimport --quiet --mode merge --collection customer --file ./results/$i/customer---$n.json --db ycsb --username guest --password guest --authenticationDatabase admin --host c1:27117
+#        mongoimport --quiet --mode merge --collection order --file ./results/$i/order---$n.json --db ycsb --username guest --password guest --authenticationDatabase admin --host c1:27117
+#    done
+#done
--- importer-json-local.sh
+++ importer-json-local.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-folders=10
-records=100
-for i in `seq 1 $folders`; do
+folders=9
+for i in `seq 0 $folders`; do
     echo "$(date) >> importing folder ${i}..."
-    for j in `seq 0 $records`; do
-        n=$((i * $records + j))
-
-        mongoimport --quiet --mode merge --collection customer --file ./results/$i/customer---$n.json --uri mongodb://localhost:27017/ycsb-local
-        mongoimport --quiet --mode merge --collection order --file ./results/$i/order---$n.json --uri mongodb://localhost:27017/ycsb-local
-    done
+    mongoimport --jsonArray --mode merge --collection customer --file ./results/$i/combined-customers.json --uri mongodb://localhost:27017/ycsb-local
+    mongoimport --jsonArray --mode merge --collection order --file ./results/$i/combined-orders.json --uri mongodb://localhost:27017/ycsb-local
 done
+
+#folders=10
+#records=100
+#for i in `seq 1 $folders`; do
+#    echo "$(date) >> importing folder ${i}..."
+#    for j in `seq 0 $records`; do
+#        n=$((i * $records + j))
+#
+#        mongoimport --quiet --mode merge --collection customer --file ./results/$i/customer---$n.json --uri mongodb://localhost:27017/ycsb-local
+#        mongoimport --quiet --mode merge --collection order --file ./results/$i/order---$n.json --uri mongodb://localhost:27017/ycsb-local
+#    done
+#done
