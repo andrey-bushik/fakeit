0706622935a11708380c22e36b09a8f098a6fbaf
Andrey Bushik <andrey.bushik@gmail.com>
2018-06-27 02:23:06 +0300

    fix jq 'too many arguments error'

--- combined-json.sh
+++ combined-json.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 for filepath in $(ls -d results/*/); do
-  jq -c -s '.' $filepath/customer*.json > $filepath/combined-customers.json
-  jq -c -s '.' $filepath/order*.json > $filepath/combined-orders.json
+#  jq -c -s '.' $filepath/customer*.json > $filepath/combined-customers.json
+#  jq -c -s '.' $filepath/order*.json > $filepath/combined-orders.json
+
+  find $filepath/customer*.json -maxdepth 1 -exec cat {} + | jq -s . > $filepath/combined-customers.json
+  find $filepath/order*.json -maxdepth 1 -exec cat {} + | jq -s . > $filepath/combined-orders.json
 done
