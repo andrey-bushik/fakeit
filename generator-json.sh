74773ebe2e41a853013f5e3ce123902daf2a3a9c
Andrey Bushik <andrey.bushik@gmail.com>
2018-06-26 09:23:23 +0300

    generate 50 mil records

--- generator-json.sh
+++ generator-json.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for i in `seq 0 99`; do
+for i in `seq 0 999`; do
     n=$((i * 50000))
     sed "s/%customer_id%/${n}/g" ./models/customers_template.yaml > ./models/customers_generated.yaml
     sed "s/%order_id%/${n}/g" ./models/orders_template.yaml > ./models/orders_generated.yaml
