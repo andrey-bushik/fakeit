a8700ea1c06acd3bdb0f74eacc1553838c5a466b
Andrey Bushik <andrey.bushik@gmail.com>
2018-06-26 18:49:43 +0300

    update generators: move values to variables

--- generator-json-local.sh
+++ generator-json-local.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-for i in `seq 0 1`; do
-    n=$((i * 3))
+folders=10
+records=100
+for i in `seq 0 $folders`; do
+    n=$((i * $records))
     sed "s/%customer_id%/${n}/g" ./models/customers_template.yaml > ./models/customers_generated.yaml
     sed "s/%order_id%/${n}/g" ./models/orders_template.yaml > ./models/orders_generated.yaml
 
-    echo " >> generating customers and orders for ids from ${n}"
-    bin/fakeit -m ./models/customers_generated.yaml,./models/orders_generated.yaml -n 3 -o json -d ./results/$i
-    echo " >> done for ids starting from ${n}"
+    echo "$(date) >> generating customers and orders for ids from ${n}"
+    bin/fakeit -m ./models/customers_generated.yaml,./models/orders_generated.yaml -n $records -o json -d ./results/$i
 done
