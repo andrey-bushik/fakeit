6bd87c68f837be8a8e35397aa08008df07bcfb13
Andrey Bushik <andrey.bushik@gmail.com>
2018-06-27 07:56:04 +0300

    attemp to fix 'too many arguments' error

--- combined-json.sh
+++ combined-json.sh
@@ -1,8 +1,33 @@
 #!/usr/bin/env bash
-for filepath in $(ls -d results/*/); do
-#  jq -c -s '.' $filepath/customer*.json > $filepath/combined-customers.json
-#  jq -c -s '.' $filepath/order*.json > $filepath/combined-orders.json
+for filepath in $(ls -d results/*); do
+  find $filepath -type f -name 'customer*.json' -exec cat {} + | jq -c -s . > $filepath/combined-customers.json
+  find $filepath -type f -name 'order*.json' -exec cat {} + | jq -c -s . > $filepath/combined-orders.json
 
-  find $filepath/customer*.json -maxdepth 1 -exec cat {} + | jq -s . > $filepath/combined-customers.json
-  find $filepath/order*.json -maxdepth 1 -exec cat {} + | jq -s . > $filepath/combined-orders.json
+#  for i in *; do cp "$i" ../prjshp/; done
+#  > $filepath/combined-customers.json
+#  > $filepath/combined-orders.json
+
+#  find -maxdepth 1 -name \*.json -exec cat {} + | jq -s . > output.json
+
+#  jq -s '{} * .[1]' $filepath/combined-customers.json >> >> $filepath/combined-customers.json
+#  find $directory -type f -name '*' -exec mv {} $directory2/. \;
+
+#  find $filepath -maxdepth 1 -name "customer*.json" | jq -f -c -s '.' > $filepath/combined-customers.json
+#   jq --arg x "${api_final}" '.item = $x'
+#
+#   zargs $filepath/customer*.json -- cat | jq -s . > $filepath/combined-customers.json
+#   zargs $filepath/order*.json -- cat | jq -s . > $filepath/combined-orders.json
+
+#  xargs $filepath/customer*.json -- cat | jq -f -c -s . > $filepath/combined-customers.json
+#  xargs $filepath/order*.json -- cat | jq -f -c -s . > $filepath/combined-orders.json
+
+#  find $filepath -name 'customer*.json' -exec jq -c -s '{}' + > $filepath/combined-customers.json
+#  find $filepath -name 'order*.json' -exec jq -c -s '{}' + > $filepath/combined-orders.json
+
+#  find . -name '$filepath/customer*.json' -exec cat '{}' + | jq -f -c -s '.' > $filepath/combined-customers.json
+#  find . -name '$filepath/order*.json' -exec cat '{}' + | jq -f -c -s '.' > $filepath/combined-orders.json
+#  jq -f -c -s '.' $filepath/customer*.json > $filepath/combined-customers.json
+#  jq -f -c -s '.' $filepath/order*.json > $filepath/combined-orders.json
+#
+#
 done
